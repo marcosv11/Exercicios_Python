57b484ff4cc88cc9dda93025a9dcb71449512d94
Marcos Vinicius <jacintomarcos1@hotmail.com>
2021-07-12 23:55:19 -0300

    jogo da forca

--- jogo/jogos.py
+++ jogo/jogos.py
@@ -1,21 +1,17 @@
 import forca
-import adivinhacao
+import adivinha
 
-def escolhe_jogo():
-    print("*********************************")
-    print("*******Escolha o seu jogo!*******")
-    print("*********************************")
+print("*********************************")
+print("Escolha o seu jogo!")
+print("*********************************")
 
-    print("(1) Forca (2) Adivinhação")
+print("(1) Forca (2) Adivinhação")
 
-    jogo = int(input("Qual jogo? "))
+jogo = int(input("Qual jogo?"))
 
-    if(jogo == 1):
-        print("Jogando forca")
-        forca.jogar()
-    elif(jogo == 2):
-        print("Jogando adivinhação")
-        adivinhacao.jogar()
-
-if(__name__ == "__main__"):
-    escolhe_jogo()
+if (jogo == 1):
+    print("Jogando forca")
+    forca.jogar()
+elif (jogo == 2):
+    print("Jogando adivinhação")
+    adivinha.jogar()
